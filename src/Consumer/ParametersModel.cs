0dac6006cc2b2d4ed0676716bfd53d0d5c27f586
Paulo Pitte <pauloanalista@outlook.com>
2025-04-11 17:22:24 -0300

    upcode

--- src/Consumer/ParametersModel.cs
+++ src/Consumer/ParametersModel.cs
@@ -1,16 +1,14 @@
-namespace Kafka.Consumer.Console
+namespace Kafka.Consumer.Console;
+public class ParametersModel
 {
-    public class ParametersModel
+    public ParametersModel()
     {
-        public ParametersModel()
-        {
-            BootstrapServer = "localhost:9092";
-            TopicName = "topic1";
-            GroupId = "Group 1";
-        }
-
-        public string BootstrapServer { get; set; }
-        public string TopicName { get; set; }
-        public string GroupId { get; set; }
+        BootstrapServer = "localhost:9092";
+        TopicName = "topic1";
+        GroupId = "Group 1";
     }
+
+    public string BootstrapServer { get; set; }
+    public string TopicName { get; set; }
+    public string GroupId { get; set; }
 }
--- src/Consumer/PessoaModel.cs
+++ src/Consumer/PessoaModel.cs
@@ -1,22 +1,19 @@
 using System.Text.Json.Serialization;
-
-namespace Kafka.Consumer.Console
+namespace Kafka.Consumer.Console;
+public record PessoaModel
 {
-    public class PessoaModel
+    [JsonPropertyName("Nome")]
+    public string? Nome { get; set; }
+    [JsonPropertyName("Idade")]
+    public int Idade { get; set; }
+
+    public override string ToString()
     {
-        [JsonPropertyName("Nome")]
-        public string? Nome { get; set; }
-        [JsonPropertyName("Idade")]
-        public int Idade { get; set; }
+        return $"Seu nome é {this.Nome} e sua idade é {this.Idade}";
+    }
 
-        public override string ToString()
-        {
-            return $"Seu nome é {this.Nome} e sua idade é {this.Idade}";
-        }
+    public void Action()
+    {
 
-        public void Action()
-        {
-            
-        }
     }
 }
