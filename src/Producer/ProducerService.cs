07f36b82b5a13561136d57d28bc52c156a103cbc
Paulo Pitte <pauloanalista@outlook.com>
2025-04-11 10:41:11 -0300

    DEMO Producer

--- src/Producer/ProducerService.cs
+++ src/Producer/ProducerService.cs
@@ -5,6 +5,7 @@ namespace Kafka.Producer.API
 {
     public class ProducerService
     {
+        private const string CRITICAL_MESSAGE = "FALHA CRITICA AO ENVIAR MENSAGEM";
         private readonly IConfiguration _configuration;
         private readonly ProducerConfig _producerConfig;
         private readonly ILogger<ProducerService> _logger;
@@ -26,19 +27,23 @@ namespace Kafka.Producer.API
             var topic = _configuration.GetSection("KafkaConfig").GetSection("TopicName").Value;
             try
             {
-                using (var producer = new ProducerBuilder<Null, string>(_producerConfig).Build())
+                if (string.IsNullOrEmpty(topic))
                 {
-                    var message = JsonSerializer.Serialize(pessoa);
-                    var result = await producer.ProduceAsync(topic: topic, new() { Value = message });
-                    _logger.LogInformation(result.Status.ToString() + " - " + message);
-
-                    return result.Status.ToString() + " - " + message;
+                    _logger.LogCritical($"{CRITICAL_MESSAGE} - TOPICO NAO ENCONTRADO");
+                    return ($"{CRITICAL_MESSAGE} - TOPICO NAO ENCONTRADO");
                 }
+                
+                using var producer = new ProducerBuilder<Null, string>(_producerConfig).Build();
+                var message = JsonSerializer.Serialize(pessoa);            
+                var result = await producer.ProduceAsync(topic: topic, new() { Value = message });
+                _logger.LogInformation(result.Status.ToString() + " - " + message);
+
+                return result.Status.ToString() + " - " + message;
             }
             catch
             {
-                _logger.LogError("Erro ao enviar mensagem");
-                return "Erro ao enviar mensagem";
+                _logger.LogCritical(CRITICAL_MESSAGE);
+                return CRITICAL_MESSAGE;
             }
         }
     }
